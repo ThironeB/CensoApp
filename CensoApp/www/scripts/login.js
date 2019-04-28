6061e1efab02bff6354167ca18fb06c16c155960
ThironeB <bermudezth@gmail.com>
2019-04-28 19:18:24 -0400

    Pantallas login, sign in, animaciones entre pantallas, lista de familias

---
+++ CensoApp/www/scripts/login.js
@@ -0,0 +1,12 @@
+﻿$('#LoginForm').on("submit", function (e) {
+    e.preventDefault();
+
+    $("#btnIndexTranslate").click();
+    
+    //$("body").fadeOut(1000, redireccionarPag);
+});
+
+//function redireccionarPag() {
+//    var url = "index.html";
+//    $(location).attr('href', url);
+//}
